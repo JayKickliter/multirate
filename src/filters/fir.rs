fc9cdf2a8f21fc9aa5f7dab241005ab28d8f0c65
Jay Kickliter <jay@kickliter.com>
2022-05-24 13:25:30 -0600

    Add basic FIR functionality

--- src/filters/fir.rs
+++ src/filters/fir.rs
@@ -1,3 +1,64 @@
-pub struct FIR<Ts, Tt> {}
+use crate::{collections::queue::Queue, math::dotprod::dot};
+use core::ops::Mul;
+use num_traits::Zero;
 
-impl<Ts, Tt> FIR<Ts, Tt> {}
+/// A stateful, streaming, FIR filter.
+pub struct FIR<H, X> {
+    /// Filter taps stored in reverse-order.
+    h: Box<[H]>,
+    /// Previous input samples.
+    history: Queue<X>,
+}
+
+impl<H, X, O> FIR<H, X>
+where
+    H: Mul<X, Output = O>,
+    X: Copy,
+{
+    pub fn with_taps(taps: &[H]) -> Self
+    where
+        H: Clone,
+        X: Zero,
+    {
+        Self {
+            h: taps.iter().rev().cloned().collect(),
+            history: Queue::with_capacity(taps.len()),
+        }
+    }
+
+    pub fn exec(&mut self, x: X) -> O
+    where
+        H: Copy,
+        O: Zero,
+    {
+        self.history.push(x);
+        let (x0, x1) = self.history.as_slices();
+        let (h0, h1) = self.h.split_at(x0.len());
+        dot(h0, x0) + dot(h1, x1)
+    }
+
+    pub fn h(&self) -> &[H] {
+        &self.h
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fir_create() {
+        let taps = &[1.0_f32, 2.0, 3.0];
+        let filt: FIR<f32, f32> = FIR::with_taps(taps);
+        assert_eq!(filt.h(), &[3.0_f32, 2.0, 1.0]);
+    }
+
+    #[test]
+    fn test_fir_exec() {
+        let taps = &[1, 2, 3];
+        let mut filt: FIR<i32, i32> = FIR::with_taps(taps);
+        let xs = &[1, 0, 0, 0];
+        let ys: Vec<i32> = xs.iter().map(|x| filt.exec(*x)).collect();
+        assert_eq!(ys, [1, 2, 3, 0]);
+    }
+}
